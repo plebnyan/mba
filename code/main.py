839b4f8e95bdd45e883ab9e55a5ef5cb3268eb4d
Nyan Lynn Tun <nlt1166@outlook.com>
2023-10-26 15:48:28 +0630

    Add files via upload

---
+++ code/main.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+
+data = pd.read_excel(r"D:\Scrpits\RoadShow\result.xlsx")
+
+
+
+
+selected_antecedent = st.selectbox('Select an Item', data['item1'].unique())
+
+# Filter the data based on the selected antecedent
+filtered_data = data[data['item1'] == selected_antecedent]
+filtered_data['support']=(filtered_data['support'] * 100).apply(lambda x: f'{x:.2f}%')
+filtered_data = filtered_data.sort_values(by='support', ascending=False)
+
+# Display the table of consequents with support
+st.dataframe(filtered_data[['item2', 'support']],hide_index=True,width=800)
+
+
+
+
+
+
+
+
+
